c14f53237940697f2600f5d80ea6d7c01ff4ce8b
Derek <derekbeaton@users.noreply.github.com>
2018-02-08 16:43:47 -0500

    Changes to make.data.nominal and DESCRIPTION

--- OuRS/R/make.data.nominal.R
+++ OuRS/R/make.data.nominal.R
@@ -1,13 +1,8 @@
 ## I need a much better way of doing this...
   ## also I need to provide the option of filling NAs in with the barycenters/means.
   ### that should not be the default.
-make.data.nominal <- function(datain){
+make.data.nominal <- function(datain,impute.NA.to.mean=T){
 
-
-  num.nas <- sum(is.na(datain))
-  if (num.nas > 0) {
-    print(paste("You have ", num.nas, " NAs in your data. makeNominalData automatically imputes NA with the mean of the columns.", sep = ""))
-  }
 
   data_dims <- dim(datain)
   var_names <- colnames(datain)
@@ -16,7 +11,7 @@ make.data.nominal <- function(datain){
   new.col.count <- sum(apply(datain,2,function(x){ uniq <- unique(x); length(uniq) - sum(is.na(uniq))	}))
   dataout <- matrix(0,nrow(datain),new.col.count)
   beginner <- 0
-  new_colnames <- matrix(0, 1, 0)
+  variable.map <- new_colnames <- matrix(0, 1, 0)
   for (i in 1:data_dims[2]) {
     unique_elements <- unique(datain[, i])
     unique_no_na <- unique_elements[!is.na(unique_elements)]
@@ -24,19 +19,28 @@ make.data.nominal <- function(datain){
     for (j in 1:ncol(mini.mat)) {
       mini.mat[which(datain[, i] == unique_no_na[j]), j] <- 1
       new_colnames <- cbind(new_colnames, paste(var_names[i], ".", unique_no_na[j], sep = ""))
+      variable.map <- cbind(variable.map,var_names[i])
     }
 
+    ## here I need to be able to allow for these to remain as NA.
     these.missing <- which(rowSums(mini.mat)==0)
     if(length(these.missing)>0){
-      barycenter <- colSums(mini.mat)/sum(colSums(mini.mat))
-      mini.mat[these.missing,] <- matrix(barycenter,length(these.missing),length(barycenter),byrow=T)
+      if(impute.NA.to.mean){
+        replacement <- colSums(mini.mat)/sum(colSums(mini.mat))
+      }else{
+        replacement <- rep(NA,ncol(mini.mat))
+      }
+      mini.mat[these.missing,] <- matrix(replacement,length(these.missing),length(replacement),byrow=T)
     }
 
     ender <- beginner + length(unique_no_na)
     dataout[, (beginner + 1):ender] <- mini.mat
     beginner <- ender
   }
-  colnames(dataout) <- new_colnames
+  colnames(dataout) <- c(new_colnames)
   rownames(dataout) <- ind_names
+  attributes(dataout)$variable.map <- c(variable.map)
+  class(dataout) <- "matrix"
+
   return(as.matrix(dataout))
 }
