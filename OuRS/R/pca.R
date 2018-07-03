0748df023bb68a7690dd00cd6379da2dd90d216b
Derek <derekbeaton@users.noreply.github.com>
2018-07-03 14:36:53 -0400

    Created simple PCA for use.
    Stolen from SlimPosition for use in our OuRS pipeline.

---
+++ OuRS/R/pca.R
@@ -0,0 +1,11 @@
+## Stolen primarily from SlimPosition (https://github.com/derekbeaton/ExPosition-Family/blob/master/ExPosition2/SlimPosition/Package/R/sp.pca.R)
+## Compact will be only: fi, fj, u, v, and d.orig
+sp.pca <- function(DATA, center = T, scale = "SS1", k = 0, compact = T, graphs = F){
+
+  res <- gsvd(expo.scale(DATA, center = center, scale = scale), k = k)
+  if(compact){
+    res <- list(fi=res$fi, fj=res$fj, d.orig=res$d.orig, u=res$u, v=res$v)
+  }
+  
+  return(res)
+}
