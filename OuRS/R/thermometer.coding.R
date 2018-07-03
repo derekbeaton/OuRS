48d5fe3c99c123889f55f2b4c56ab2005591b4ae
Derek <derekbeaton@users.noreply.github.com>
2018-07-03 14:42:04 -0400

    slight update

--- OuRS/R/thermometer.coding.R
+++ OuRS/R/thermometer.coding.R
@@ -48,9 +48,9 @@ thermometer.coding <- function(DATA, mins, maxs, norm.to.one = T){
   colnames(DATA) <- dat.col.names
 
   ## these should be normed so that the variables = 1.
-  attributes(DATA)$variable.map <- gsub("\\-","",gsub("\\+","",dat.col.names))
   class(DATA) <- "matrix"
-  
+  attributes(DATA)$variable.map <- gsub("\\-","",gsub("\\+","",dat.col.names))
+    
   return(DATA)
 }
 
