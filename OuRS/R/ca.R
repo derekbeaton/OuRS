a577975af2089731484cdf84b9c299a5688c4cae
Derek <derekbeaton@users.noreply.github.com>
2018-07-03 14:35:24 -0400

    update to CA
    Code lifted from SlimPosition.

--- OuRS/R/ca.R
+++ OuRS/R/ca.R
@@ -1,12 +1,18 @@
 
-ca <- function(X,k=0){
+## update to CA. stolen primarily from SlimPosition (https://github.com/derekbeaton/ExPosition-Family/blob/master/ExPosition2/SlimPosition/Package/R/sp.ca.R)
 
-  sum.data <- sum(X)
-  rowSums.data <- rowSums(X)
+ca <- function(DATA, k = 0, compact = T){
+
+  sum.data <- sum(DATA)
+  rowSums.data <- rowSums(DATA)
   wi <- rowSums.data/sum.data
-  wj <- colSums(X)/sum.data
-  ca.res <- gsvd( sweep(sweep(X,1,rowSums.data,"/"),2,wj), wi, 1/wj, k = k )
-  ca.res$fi <- sweep(ca.res$fi,1,wi,"/")
+  wj <- colSums(DATA)/sum.data
 
-  return( ca.res )
+  res <- gsvd( sweep(sweep(DATA,1,rowSums.data,"/"),2,wj), wi, 1/wj, k = k )
+  res$fi <- sweep(res$fi,1,wi,"/")
+
+  if(compact){
+    res <- list(fi=res$fi, fj=res$fj, d.orig=res$d.orig, u=res$u, v=res$v)
+  }
+  return(res)
 }
