acf10567b35b621e4c8dbfef079ca54d991a7b8d
Derek <derekbeaton@users.noreply.github.com>
2018-07-04 11:31:05 -0400

    stolen from SlimPosition

---
+++ OuRS/R/component.plot.R
@@ -0,0 +1,19 @@
+component.plot <- function(scores, axes=c(1,2), pch=20, col="mediumorchid4", line.col="grey80", lty=2, lwd=2,
+                              main="Component scores",
+                              xlab=paste0("Component ",axes[1]),
+                              ylab=paste0("Component ",axes[2]),
+                              xlim=c(-max(abs(scores[,axes])),max(abs(scores[,axes])))*1.2,
+                              ylim=c(-max(abs(scores[,axes])),max(abs(scores[,axes])))*1.2,
+                              asp=1, pos=3, display_names=T,cex=1,text.cex=1,
+                              ...){
+
+  plot(0, type="n", xlim=xlim, ylim=ylim, main=main, xlab=xlab, ylab=ylab, axes=F, asp=asp)
+  abline(h=0,lty=2,lwd=2, col="grey60")
+  abline(v=0,lty=2,lwd=2, col="grey60")
+  points(scores[,axes], col=col, pch=pch, cex=cex, ...)
+    ## will try to employ a "repel" later.
+  if(display_names){
+    text(scores[,axes],labels=rownames(scores),pos=pos,col=col,cex=text.cex)
+  }
+
+}
