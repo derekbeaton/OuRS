b1c44df2108bc19cceb9480efa3a96f80a9a1bac
Derek <derekbeaton@users.noreply.github.com>
2018-07-04 15:02:23 -0400

    hopefully finished with display_names vectorize

--- OuRS/R/component.plot.R
+++ OuRS/R/component.plot.R
@@ -12,21 +12,23 @@ component.plot <- function(scores, axes=c(1,2), pch=20, col="mediumorchid4", lin
   abline(v=0,lty=2,lwd=2, col="grey60")
   points(scores[,axes], col=col, pch=pch, cex=cex, ...)
     ## will try to employ a "repel" later.
+  
   ### actually, display names should be a vector
   if(length(display_names)==1){
     display_names <- rep(display_names,nrow(scores))
-  }else if(length(display_names)==nrow(scores)){
-    ## it's good.
-  }else{
-      ## a default.
-    display_names <- rep(display_names,nrow(scores))
+  }else if(length(display_names)!=nrow(scores)){
+    ## a default.
+    warning("length(display_names) is not 1 or nrow(scores). Setting to default.")
+    display_names <- rep(T,nrow(scores))
   }
-  if(all(lapply(display_names,is.logical))){
-  }else{
+    
+  if( !all(lapply(display_names,is.logical)) | any(is.na(display_names)) ){
+    warning("Not all items in 'display_names' were TRUE or FALSE")
     display_names <- rep(T,nrow(scores))
   }
   # test if display_names is all logical else set to length of vector as false
   
+    ## this might get weird if there is only 1.
   if(sum(display_names)>0){
     text(scores[which(display_names),axes],labels=rownames(scores)[which(display_names)],pos=pos,col=col,cex=text.cex)
   }
