ea4bc82dff1b42b02c81cffb577b6a9874436172
Derek <derekbeaton@users.noreply.github.com>
2018-10-14 09:57:54 -0400

    rpca updated and ready to push.

--- OuRS/R/rpca.R
+++ OuRS/R/rpca.R
@@ -2,16 +2,12 @@
   ## Effectively rewritten as a combination of: https://github.com/dlaptev/RobustPCA/blob/master/RobustPCA.m & 'rpca' package
 
 
-rpca <- function(DATA, lambda = 1 / sqrt(max(dim(DATA))), mu = 10 * lambda, maximum.iterations = 1000, tol=sqrt(.Machine$double.eps)){#, switcher = T){
+my.rpca <- function(DATA, lambda = 1 / sqrt(max(dim(DATA))), mu = 10 * lambda, maximum.iterations = 1000, tol=sqrt(.Machine$double.eps)){
 
   shrinkage.operator <- function(X, tau){
 
     return( sign(X) * pmax( abs(X) - tau, 0 ) )
 
-  }
-  reg.svd <- function(X, tau, tol=tol=sqrt(.Machine$double.eps)){
-    res <- tolerance.svd(X, tol=tol)
-    return( sweep(res$u, 2, shrinkage.operator(res$d, tau), "*") %*% t(res$v) )
   }
 
   stopping.condition <- tol * sqrt(sum(DATA^2))
@@ -21,24 +17,17 @@ rpca <- function(DATA, lambda = 1 / sqrt(max(dim(DATA))), mu = 10 * lambda, maxi
   L <- S <- Y <- matrix(0, nrow(DATA), ncol(DATA))
 
   for(i in 1:maximum.iterations){
-    # if(!switcher){
-    #   L <- reg.svd(DATA - S + ((1/mu)*Y), 1/mu)
-    # }else{
-      L <- reg.svd(DATA - S + Y, 1/mu)
-    #}
+
+    res <- tolerance.svd(DATA - S + Y, tol=tol)
+    L <- sweep(res$u, 2, shrinkage.operator(res$d, 1/mu), "*") %*% t(res$v)
 
     S <- shrinkage.operator( DATA - L + Y, lambda/mu)
     Z <- DATA - L - S
 
-    if(sum(sqrt(Z)) < stopping.condition){
+    if(sqrt(sum(Z^2)) < stopping.condition){
       break
     }
-
-    # if(!switcher){
-    #   Y <- Y + mu*Z
-    # }else{
-      Y <- Y + Z
-    #}
+    Y <- Y + Z
   }
-
+  return(list(L=L, S=S, i=i))
 }
