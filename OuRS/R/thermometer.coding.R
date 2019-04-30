9d9f0ea402966830b66e9d82c154b8a15eed7479
Derek <derekbeaton@users.noreply.github.com>
2019-04-30 13:27:17 -0400

    wtf was I thinking when I wrote this the first time?
    thermometer coding is now fixed (and was just bizarre before).

--- OuRS/R/thermometer.coding.R
+++ OuRS/R/thermometer.coding.R
@@ -1,55 +1,25 @@
-thermometer.coding <- function(DATA, mins, maxs, norm.to.one = T){
-
-  if(missing(mins)){
-    mins <- apply(DATA,2,min,na.rm=T)
-  }else{
-
-    if(length(mins)==ncol(DATA)){
-      min.test <- mins > apply(DATA,2,min,na.rm=T)
-      if(any(min.test)){
-        warning("Some inputted minimums are greater than minimums in the data. We will replace those 'mins' with their respective minimum in the data")
-        mins[which(min.test)] <- apply(DATA[,which(min.test)],2,min,na.rm=T)
-      }
-    }else{
-      mins <- apply(DATA,2,min,na.rm=T)
-    }
-
-  }
-
-
-  if(missing(maxs)){
-    maxs <- apply(DATA,2,max,na.rm=T)
-  }else{
-
-    if(length(maxs)==ncol(DATA)){
-      max.test <- maxs < apply(DATA,2,max,na.rm=T)
-      if(any(max.test)){
-        warning("Some inputted maximums are smaller than maximums in the data. We will replace those 'maxs' with their respective maximum in the data")
-        maxs[which(max.test)] <- apply(DATA[,which(max.test)],2,max,na.rm=T)
-      }
-    }else{
-      maxs <- apply(DATA,2,max,na.rm=T)
-    }
-
-  }
+thermometer.coding <- function(DATA){
+  ## radically re-written and consequently simplified.
+  ## the previous version was not correct.
 
   if(is.null(colnames(DATA))){
     warning("'colnames(DATA)' were NULL. Setting to 1:ncol(DATA).")
     colnames(DATA) <- as.character(1:ncol(DATA))
   }
-  # if(is.null(rownames(DATA))){
-  #   warning("'rownames(DATA)' were NULL. Setting to 1:nrow(DATA).")
-  #   rownames(DATA) <- as.character(1:nrow(DATA))
-  # }
 
-  dat.col.names <- c(paste0(colnames(DATA),"-"),paste0(colnames(DATA),"+"))
-  DATA <- cbind( sweep(DATA,2,maxs,"-")*-1 , sweep(DATA,2,mins,"-"))
-  if(norm.to.one){
-    DATA <- sweep(DATA,1,rowSums(DATA),"/")
-  }
+  ## enforce proper thermometer: subtract the maxs and make this easy.
+  DATA <- apply(DATA, 2, function(x){ max(x, na.rm = T)-x })
+  mins <- apply(DATA, 2, min, na.rm=T)
+  maxs <- apply(DATA, 2, max, na.rm=T)
+  dat.col.names <- c(paste0(colnames(DATA),"+"),paste0(colnames(DATA),"-"))
+
+  DATA <- as.matrix(
+    cbind(
+      sweep(sweep(DATA,2,maxs,"-")*-1,2,maxs,"/"),
+      sweep(sweep(DATA,2,mins,"-"),2,maxs,"/")
+      )
+    )
   colnames(DATA) <- dat.col.names
-
-  DATA <- as.matrix(DATA)
   attributes(DATA)$variable.map <- gsub("\\-","",gsub("\\+","",dat.col.names))
 
   return(DATA)
