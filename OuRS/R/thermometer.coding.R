22518d6bbe79060e4d531aeac2b1f216539a4453
Derek <derekbeaton@users.noreply.github.com>
2019-09-16 14:06:15 -0400

    again: wtf was I thinking before?
    this should be a much more suitable update to thermometer coding.

--- OuRS/R/thermometer.coding.R
+++ OuRS/R/thermometer.coding.R
@@ -1,26 +1,37 @@
-thermometer.coding <- function(DATA){
-  ## radically re-written and consequently simplified.
-  ## the previous version was not correct.
-
-  if(is.null(colnames(DATA))){
+thermometer.coding <- function (DATA, mins, maxs)
+{
+  if (is.null(colnames(DATA))) {
     warning("'colnames(DATA)' were NULL. Setting to 1:ncol(DATA).")
     colnames(DATA) <- as.character(1:ncol(DATA))
   }
 
-  ## enforce proper thermometer: subtract the maxs and make this easy.
-  DATA <- apply(DATA, 2, function(x){ max(x, na.rm = T)-x })
-  mins <- apply(DATA, 2, min, na.rm=T)
-  maxs <- apply(DATA, 2, max, na.rm=T)
-  dat.col.names <- c(paste0(colnames(DATA),"+"),paste0(colnames(DATA),"-"))
+  dat.col.names <- c(paste0(colnames(DATA), "+"), paste0(colnames(DATA), "-"))
 
-  DATA <- as.matrix(
-    cbind(
-      sweep(sweep(DATA,2,maxs,"-")*-1,2,maxs,"/"),
-      sweep(sweep(DATA,2,mins,"-"),2,maxs,"/")
-      )
-    )
-  colnames(DATA) <- dat.col.names
-  attributes(DATA)$variable.map <- gsub("\\-","",gsub("\\+","",dat.col.names))
+  ## these need a lot of checks:
+    ### and these need to be cleaned up substantially...
+  if(missing(maxs)){
+    maxs <- apply(DATA, 2, max, na.rm = T)
+  }
+  if(length(maxs)!=ncol(DATA)){
+    maxs <- apply(DATA, 2, max, na.rm = T)
+  }
+  if(any( maxs < apply(DATA, 2, max, na.rm = T))){
+    maxs <- apply(DATA, 2, max, na.rm = T)
+  }
 
-  return(DATA)
+  if(missing(mins)){
+    mins <- apply(DATA, 2, min, na.rm = T)
+  }
+  if(length(mins)!=ncol(DATA)){
+    mins <- apply(DATA, 2, min, na.rm = T)
+  }
+  if(any( mins > apply(DATA, 2, min, na.rm = T))){
+    mins <- apply(DATA, 2, min, na.rm = T)
+  }
+  ## clean the above.
+
+  DATAOUT <- sweep(cbind(sweep(DATA, 2, maxs, "-") * -1, sweep(DATA, 2, mins, "-")),2, (maxs-mins), "/")
+  colnames(DATAOUT) <- dat.col.names
+  attributes(DATAOUT)$variable.map <- gsub("\\-", "", gsub("\\+", "", dat.col.names))
+  return(DATAOUT)
 }
