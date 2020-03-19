772bd9509236c6003d58526e136d2ae550e6a757
Derek <derekbeaton@users.noreply.github.com>
2020-03-19 10:38:53 -0400

    some overhauls happening

--- tests/CatMCD_Test.R
+++
@@ -1,4 +0,0 @@
-data(SNPS)
-
-snps.res <- cat.mcd(SNPS,make.data.disjunctive = T,num.subsets = 500,alpha = .5)
-
--- tests/CatMCD_corrmax.R
+++
@@ -1,6 +0,0 @@
-data(SNPS)
-
-snps.res <- cat.mcd(SNPS,make.data.disjunctive = T,num.subsets = 500,alpha = .5)
-ca.preproc.res <- ca.preproc(make.data.nominal(SNPS))
-
-corrmax.res <- cat.corrmax(ca.preproc.res$weightedZx, loadings = snps.res$cov$loadings,singular.values = snps.res$cov$singular.values)
