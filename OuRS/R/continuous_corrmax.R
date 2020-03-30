12889afdcdc5cfe719107cef0598bea7da9fdf46
Derek <derekbeaton@users.noreply.github.com>
2020-03-29 23:29:02 -0400

    and now some corrmax stuff

--- OuRS/R/continuous_corrmax.R
+++ OuRS/R/continuous_corrmax.R
@@ -5,28 +5,36 @@
 
 
 #cont.corrmax <- function(population.data,sample.data){
-continous_corrmax <- function(target.data,rob.center=T,rob.scale=F,loadings,singular.values,tol=.Machine$double.eps){
 
+#' @title CorrMax transformation
+#' 
+#' @description A transformation of the data in a row-wise fashion that preserves the Mahalanobis distances yet also provides information on which variables likely contribute to the Mahalanobis distance of the observation
+#' 
+#' @param target.data
+#' @param rob.center
+#' 
+continous_corrmax <- function(target.data, center=T, scale=F, loadings, singular.values, tol=.Machine$double.eps){
 
-  target.data <- ours_scale(target.data,center=rob.center,scale=rob.scale)
-
+  target.data <- ours_scale(target.data, center=center,scale=scale)
+  
+  if(missing(loadings) | missing(singular.values)){
+    
+    svd.res <- tolerance_svd(target.data, tol=tol)
+    loadings <- svd.res$v
+    singular.values <- svd.res$d
+    svd.res <- NULL ## get rid of it
+    
+  }
+  
   diag.sampcov <- sqrt(diag(tcrossprod( sweep(loadings,2,singular.values,"*") )))
-
-  inv.DSD.half <- tcrossprod( GSVD::invsqrt_psd_matrix(sweep( sweep(loadings,2,singular.values,"*"),1,diag.sampcov,"/")) )
-
+    ## this can be made more efficient if I use /svd()$d as another sweep inside the tcrossprod()
+  inv.DSD.half <- GSVD::invsqrt_psd_matrix( tcrossprod( sweep( sweep(loadings,2,singular.values,"*"),1,diag.sampcov,"/") ) )
   W <- target.data %*% sweep(inv.DSD.half,1,diag.sampcov,"/")
-  w.svd <- tolerance_svd(W,tol=tol)
-    ## no need because, as in Eq 2(?) in Garthwaite & Koch (2016) they point out that X --> W exists where mahal(X) == mahal(W)
-    ## but I've brought it back for convenience
-
-    ## we should use percentages in place of contributions as they have meaning
-  #return(list(mah=rowSums(w.svd$u^2),percs=sweep(W^2,1,rowSums(W^2),"/")*100))
   
   res <- list(
-    mahal_dists = rowSums(w.svd$u^2),
-    mahal_dists2 = diag(crossprod(W)),
     corr_max_transform = inv.DSD.half,
     transformed_data = W,
+    mahal_dists = rowSums(W^2),
     percentage_contributions = sweep(W^2,1,rowSums(W^2),"/")*100
   )
   
